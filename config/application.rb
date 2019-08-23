738fbc1f4e3d09f0d0e3fb99f3f15f844fec7a31
yangyiwei <441420776@qq.com>
2019-08-23 17:52:36 +0800

    webpacker  env

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,25 @@
 require_relative 'boot'
 
-require 'rails/all'
+require "rails"
+
+%w(
+  active_record/railtie
+  active_storage/engine
+  action_controller/railtie
+  action_view/railtie
+  action_mailer/railtie
+  active_job/railtie
+  action_cable/engine
+  action_mailbox/engine
+  action_text/engine
+  rails/test_unit/railtie
+  #sprockets/railtie
+).each do |railtie|
+  begin
+    require railtie
+  rescue LoadError
+  end
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
