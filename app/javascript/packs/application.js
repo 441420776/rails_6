f7cf033f628a12376c197f4fbb50ac1d5e7de873
yangyiwei <441420776@qq.com>
2019-08-31 12:56:16 +0800

    bootstrap and font-awesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,16 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import 'jquery'
+import 'bootstrap'
+window.$ = window.jQuery = require("jquery");
+import 'stylesheets/application'
+
+document.addEventListener('turbolinks:load', ()=>{
+    $('[data-toggle="tooltip"]').tooltip()
+    $('[data-toggle="popover"]').popover()
+})
+
 
 require("@rails/ujs").start()
 require("turbolinks").start()
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'homes#index'
   get '/docs/index', to: 'docs#index'
+  resources :homes
+
 end
